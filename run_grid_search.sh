1da7d1c8f96b39deb4e325ba27b11a9d85517edb
davletov-aa <adavletov94@gmail.com>
2020-07-24 02:06:45 +0700

    bug fix

--- run_grid_search.sh
+++ run_grid_search.sh
@@ -4,5 +4,5 @@ default_test_file=$3
 
 for ((i=1; i<=100; i++));
 do
-  python execute_command.py `python yield_command.py $i $train_config_json_path $grid_json_path $default_test_file`;
+  python execute_command.py `python yield_command.py $i $train_config_json_path $grid_json_path --default_test_file $default_test_file`;
 done
