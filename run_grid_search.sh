36f1866ff2c4ae7abb7db705d1b75594944bb4c2
davletov-aa <adavletov94@gmail.com>
2020-07-24 00:06:01 +0700

    added grid for predictions

--- run_grid_search.sh
+++ run_grid_search.sh
@@ -1,7 +1,8 @@
 train_config_json_path=$1
 grid_json_path=$2
+default_test_file=$3
 
 for ((i=1; i<=100; i++));
 do
-  python execute_command.py `python yield_command.py $i $train_config_json_path $grid_json_path`;
+  python execute_command.py `python yield_command.py $i $train_config_json_path $grid_json_path $default_test_file`;
 done
