212f56d386f15ac173dae827a046e8c9d8288cdb
davletov-aa <adavletov94@gmail.com>
2020-07-23 20:13:08 +0700

    added scoring

--- score_predictions.py
+++ score_predictions.py
@@ -4,26 +4,6 @@ from utils.data_processing import *
 from glob import glob
 
 
-def score_task_12(
-    models_regex: str,
-    local_data_dir: str,
-    comment: str = '',
-    scores_dir: str = 'scores',
-    save_path: str = 'scores.json'
-):
-    pass
-
-
-def score_task_2(
-    models_regex: str,
-    local_data_dir: str,
-    comment: str = '',
-    scores_dir: str = 'scores',
-    save_path: str = 'scores.json'
-):
-    pass
-
-
 def score_task_123(
     models_regex: str,
     local_data_dir: str,
@@ -35,44 +15,30 @@ def score_task_123(
     path_to_eval_config = 'evaluation/configs/eval_test.yaml'
 
     scores = {}
-    for part in ['test', 'dev']:
-        best_task_1_predictions_regex = \
-            f'{models_regex}/{part}_best_sent_type*.tsv'
-        best_task_2_predictions_regex = \
-            f'{models_regex}/{part}_best_tags_sequence*.tsv'
-        best_task_3_predictions_regex = \
-            f'{models_regex}/{part}_best_relations_sequence*.tsv'
+    suffix = {
+        1: 'best_sent_type',
+        2: 'best_tags_sequence',
+        3: 'best_relations_sequence'
+    }
+    for task_id in [1, 2, 3]:
+        for part in ['test', 'dev']:
+            best_task_predictions_regex = \
+                f'{models_regex}/{part}_{suffix[task_id]}*.tsv'
+            print(f'best task {task_id} predictions:')
+            print(glob(best_task_predictions_regex))
 
-        print('best task 1 predictions:')
-        print(glob(best_task_1_predictions_regex))
-        print('best task 2 predictions:')
-        print(glob(best_task_2_predictions_regex))
-        print('best task 3 predictions:')
-        print(glob(best_task_3_predictions_regex))
-
-        for pool_type in ['max_score', 'ellections']:
-            scores[f'{part}-best_sent_type-{pool_type}'] = score_task_1_predictions(
-                path_to_scorer_script=path_to_scorer_script,
-                path_to_gold_data=os.path.join(local_data_dir, f'task_1/{part}'),
-                path_to_eval_config=path_to_eval_config,
-                predictions_regex=best_task_1_predictions_regex,
-                temp_output='temp_output',
-                clean_output=True,
-                scores_dir=f'{scores_dir}/task_1/{part}-123-{pool_type}-{comment}',
-                pool_type=pool_type
-            )
-
-            scores[f'{part}-best_tags_sequence-{pool_type}'] = score_task_2_predictions(
-                path_to_scorer_script=path_to_scorer_script,
-                path_to_gold_data=os.path.join(local_data_dir, f'task_2/{part}'),
-                path_to_eval_config=path_to_eval_config,
-                predictions_regex=best_task_2_predictions_regex,
-                temp_output='temp_output',
-                clean_output=True,
-                scores_dir=f'{scores_dir}/task_2/{part}-123-{pool_type}-{comment}',
-                pool_type=pool_type
-            )
-
+            for pool_type in ['max_score', 'ellections']:
+                scores[f'{part}-{suffix[task_id]}-{pool_type}'] = score_tasks_predictions(
+                    path_to_scorer_script=path_to_scorer_script,
+                    path_to_gold_data=os.path.join(local_data_dir, f'task_{task_id}/{part}'),
+                    path_to_eval_config=path_to_eval_config,
+                    predictions_regex=best_task_predictions_regex,
+                    temp_output='temp_output',
+                    clean_output=True,
+                    scores_dir=f'{scores_dir}/task_{task_id}/{part}-123-{pool_type}-{comment}',
+                    pool_type=pool_type,
+                    task_id=task_id
+                )
     json.dump(scores, open(save_path, 'w'))
     print(scores)
 
